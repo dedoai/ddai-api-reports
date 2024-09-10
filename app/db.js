02160a0ad0e58739c99e63d678e5288967090c7d
gius <giuseppe.tortorella@dedoai.org>
2024-09-10 16:15:33 +0200

    sss

--- app/db.js
+++ app/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const { getDbSecretPwd } = require('./utils')
 
 const getDbConnection = async () => {
-    const pwd = await getDbSecretPwd()
+    // const pwd = await getDbSecretPwd()
     return new Pool({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
