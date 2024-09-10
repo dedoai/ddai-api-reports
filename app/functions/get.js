457c9c0d01b452520e54eb9ebc957d546b4c3f22
gius <giuseppe.tortorella@dedoai.org>
2024-09-10 13:32:01 +0200

    a

--- app/functions/get.js
+++ app/functions/get.js
@@ -6,7 +6,7 @@ const get = async (getDTO) => {
     const value = validateGetDTO(getDTO);
     const { name, limit, offset } = value
     const db = await getDbConnection()
-    const query = 'SELECT * FROM ' + name + ' OFFSET $1 LIMIT $2'
+    const query = 'SELECT * FROM ' + name + ' OFFSET $1 LIMIT $2 '
     result = await db.query(query, [offset, limit])
 
     return result.rows.length === 1 ? result.rows[0] : result.rows
