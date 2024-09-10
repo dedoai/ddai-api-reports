dc8872f2cf662693ef77b88a00f37a29f09ead92
gius <giuseppe.tortorella@dedoai.org>
2024-09-10 17:08:45 +0200

    a

--- app/db.js
+++ app/db.js
@@ -2,12 +2,12 @@ const { Pool } = require('pg');
 const { getDbSecretPwd } = require('./utils')
 
 const getDbConnection = async () => {
-    // const pwd = await getDbSecretPwd()
+    const pwd = await getDbSecretPwd()
     return new Pool({
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
-        password: process.env.DB_PASS,
+        password: pwd,
         port: process.env.DB_PORT,
         ssl: { rejectUnauthorized: false }
     })
