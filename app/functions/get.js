4bfe532348b7f219a1b60f74b509300aef31d7f6
gius <giuseppe.tortorella@dedoai.org>
2024-09-11 11:47:40 +0200

    list on get

--- app/functions/get.js
+++ app/functions/get.js
@@ -9,7 +9,7 @@ const get = async (getDTO) => {
     const query = 'SELECT * FROM ' + name + ' OFFSET $1 LIMIT $2 '
     result = await db.query(query, [offset, limit])
 
-    return result.rows.length === 1 ? result.rows[0] : result.rows
+    return result.rows
 }
 
 module.exports = {
