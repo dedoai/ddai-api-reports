b97764921683dc6ee63b847450655f82e3bb51c8
gius <giuseppe.tortorella@dedoai.org>
2024-10-03 12:49:40 +0200

    fix: properly use of user_id column

--- app/queries.js
+++ app/queries.js
@@ -8,13 +8,13 @@ const queries = {
                          OFFSET $1 LIMIT $2`,
     best_dataset_price: `SELECT us.username as user, COALESCE(SUM(dt.price))  as total_price
                          FROM users us  inner join datasets dt 
-                         on dt.owner_id  = us.id 
+                         on dt.user_id  = us.id 
                          group by us.username 
                          order by total_price desc 
                          OFFSET $1 LIMIT $2`,
     best_rewards: `SELECT us.username as user, COALESCE(SUM(callfordata.reward)) as total_rewards
                         FROM users us  inner join c4d callfordata
-                        on callfordata.consumer_id  = us.id 
+                        on callfordata.user_id  = us.id 
                         group by us.username 
                         order  by total_rewards desc 
                         OFFSET $1 LIMIT $2 `
