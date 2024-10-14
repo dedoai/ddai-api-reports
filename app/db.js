b6031f006b4db36e17db28ade824089ab0d008c7
gius <giuseppe.tortorella@dedoai.org>
2024-10-14 10:13:53 +0200

    chore: numeric type on db

--- app/db.js
+++ app/db.js
@@ -1,4 +1,6 @@
-const { Pool } = require('pg');
+const { Pool, types } = require('pg');
+types.setTypeParser(1700, (val) => parseFloat(val));
+
 const { getDbSecretPwd } = require('./utils')
 
 const getDbConnection = async () => {
